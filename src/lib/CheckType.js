60f36d9fb6ee6ec18c413362aecb30ef7c7d5e17
Gustavo Straub <gustavostraubcovers@gmail.com>
2021-02-13 18:29:07 -0300

    BG color type check

---
+++ src/lib/CheckType.js
@@ -0,0 +1,42 @@
+export default function CheckType(type) {
+  if (type == 'normal') {
+    return '#bcbcad'
+  } else if (type == 'grass') {
+    return '#8dd851'
+  } else if (type == 'fire') {
+    return '#fa5543'
+  } else if (type == 'water') {
+    return '#55acff'
+  } else if (type == 'fighting') {
+    return '#a85645'
+  } else if (type == 'flying') {
+    return '#79a3ff'
+  } else if (type == 'poison') {
+    return '#a75c9f'
+  } else if (type == 'ground') {
+    return '#eccb56'
+  } else if (type == 'rock') {
+    return '#cebd72'
+  } else if (type == 'bug') {
+    return '#c2d21f'
+  } else if (type == 'ghost') {
+    return '#7975d7'
+  } else if (type == 'electric') {
+    return '#fde53e'
+  } else if (type == 'psychic') {
+    return '#fa65b5'
+  } else if (type == 'ice') {
+    return '#96f1ff'
+  } else if (type == 'dragon') {
+    return '#8a75ff'
+  } else if (type == 'dark') {
+    return '#896552'
+  } else if (type == 'steel') {
+    return '#c4c2da'
+  } else if (type == 'fairy') {
+    return '#f9adff'
+  } else {
+    return '#cecece'
+  }
+  
+}
